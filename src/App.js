5f7f602e11cd415e758660984269bd8f405285ee
me-aditya <adityarathi1212@gmail.com>
2020-11-01 15:34:48 +0530

    Sign up imported and styled

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ class App extends React.Component  {
         userRef.onSnapshot(snapShot => {
           this.setState({
             currentUser : {
-              id : snapShot.id(),
+              id : snapShot.id,
               ...snapShot.data()
             }
           })
